52260115d91d28d5ee0ac21cbaa02a1215a2f8c0
Daniel Diaz <127716687+DaniDRSV@users.noreply.github.com>
2023-08-12 17:45:15 -0600

    Add files via upload

---
+++ ProgramacionOrientadaEventos/Laboratorio1/Tarea1/Ejercicio2/Forms/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Ejercicio2.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculoOpuestoBoton = new System.Windows.Forms.Label();
+            this.numeroIngresado = new System.Windows.Forms.TextBox();
+            this.BotonCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CalculoOpuestoBoton
+            // 
+            this.CalculoOpuestoBoton.AutoSize = true;
+            this.CalculoOpuestoBoton.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculoOpuestoBoton.Location = new System.Drawing.Point(27, 38);
+            this.CalculoOpuestoBoton.Name = "CalculoOpuestoBoton";
+            this.CalculoOpuestoBoton.Size = new System.Drawing.Size(252, 26);
+            this.CalculoOpuestoBoton.TabIndex = 0;
+            this.CalculoOpuestoBoton.Text = "Calcular numero opuesto";
+            // 
+            // numeroIngresado
+            // 
+            this.numeroIngresado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numeroIngresado.Location = new System.Drawing.Point(92, 85);
+            this.numeroIngresado.Name = "numeroIngresado";
+            this.numeroIngresado.Size = new System.Drawing.Size(126, 22);
+            this.numeroIngresado.TabIndex = 1;
+            this.numeroIngresado.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numeroIngresado_KeyPress);
+            // 
+            // BotonCalcular
+            // 
+            this.BotonCalcular.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonCalcular.Location = new System.Drawing.Point(111, 123);
+            this.BotonCalcular.Name = "BotonCalcular";
+            this.BotonCalcular.Size = new System.Drawing.Size(85, 33);
+            this.BotonCalcular.TabIndex = 2;
+            this.BotonCalcular.Text = "Calcular";
+            this.BotonCalcular.UseVisualStyleBackColor = true;
+            this.BotonCalcular.Click += new System.EventHandler(this.BotonCalcular_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 218);
+            this.Controls.Add(this.BotonCalcular);
+            this.Controls.Add(this.numeroIngresado);
+            this.Controls.Add(this.CalculoOpuestoBoton);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "k";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CalculoOpuestoBoton;
+        private System.Windows.Forms.TextBox numeroIngresado;
+        private System.Windows.Forms.Button BotonCalcular;
+    }
+}
---
+++ ProgramacionOrientadaEventos/Laboratorio1/Tarea1/Ejercicio2/Forms/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        //Aqui se define el evento que se ejecuta al hacer clic al boton llamado "BotonCalcular"
+        private void BotonCalcular_Click(object sender, EventArgs e)
+        {
+            double numero;
+            numero = Convert.ToDouble(numeroIngresado.Text);
+            /*Aqui se realiza la logica del calculo opuesto. Si el numero ingresado es mayor que 0
+            se calcula el opuesto multiplicador por -1 y se muestra el resultado en un cuadro
+            de mensaje*/
+            if (numero > 0)
+            {
+                double numOpuesto = numero * -1;
+                MessageBox.Show("El numero opuesto es: " + numOpuesto);
+            }
+            /* Si el numero es menor que 0, se calcula el valor absoluto y se muestra e un cuadro
+             de mensaje*/
+            else if (numero < 0)
+            {
+                double numeroNeg = System.Math.Abs(numero);
+                MessageBox.Show("El numero opuesto es: " + numeroNeg);
+            }
+            /*Si el numero es 0, se muestra un mensaje en un cuadro que es cero*/
+            else
+            {
+                MessageBox.Show("El numero es cero");
+            }
+        }
+        /* Este metodo maneja el evento de presionar una tecla en el campo de texto "numeroIngresaddo"
+         Lo que hace es verificar si la tecla presionada es una que no corresponde a un numero*/
+        private void numeroIngresado_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >=58 && e.KeyChar <=255) || (e.KeyChar >=32 && e.KeyChar <=44) || (e.KeyChar >=46 && e.KeyChar <=47))
+            {
+                e.Handled = true;
+                MessageBox.Show("Solo se permiten numeros");
+            }
+
+        }
+    }
+}
